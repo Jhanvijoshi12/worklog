17aa9283d0312ed0ede0d8152678f6b5d7cff487
Jhanvi joshi <jhanvi.joshi@trootech.com>
2023-03-03 17:07:16 +0530

    fix: changes in setting file

--- worklog/settings.py
+++ worklog/settings.py
@@ -180,5 +180,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jhanvi.joshi@trootech.com'
-EMAIL_HOST_PASSWORD = '$a-hMKDm6$M+'
+EMAIL_HOST_USER = 'admin@gmail.com'
+EMAIL_HOST_PASSWORD = '12345'
